f29905627061ca99df39f8e5c8ff8b3b55d276fe
Daniel V Mathew <danielvmathew27@gmail.com>
2024-08-02 04:45:05 +0530

    initial commit
    This is just a simple module to exec and view outputs on android from
    termux. This module will provide a way to launch the `termux-share`
    from proot evironment. There will be a server running on the actual
    termux enviroment and a client inside the proot enviroment. The client
    will signal the server to open the file.
    
    And btw, termux-share is slow from the proot environment, that's why
    this thing exists.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "pynotes",
+    packages = find_packages(["pynotes"]),
+    version = "0.1.0",
+    description = "A library exec and view output scripts in notes on android",
+    author = "Daniel",
+    install_requires = []
+)
