a3d504d7af58e5618f3885441d1a39ecae3de15c
Daniel V Mathew <danielvmathew27@gmail.com>
2024-08-02 06:16:37 +0530

    changed LaunchConnection to ViewConnection

--- pynotes/pynotes.py
+++ pynotes/pynotes.py
@@ -1,13 +1,13 @@
 import socket
 import os
 
-class LaunchConnection():
+class ViewConnection():
 
     port: int | None = None
 
     def __init__ (self, port: int | None = None) -> None:
 
-        if LaunchConnection.port is None:
-            LaunchConnection.port = port or 52886
+        if ViewConnection.port is None:
+            ViewConnection.port = port or 52886
 
         return
