b501e3bdedc35d947ae0e320b3c1386ed4693f73
Daniel V Mathew <danielvmathew27@gmail.com>
2024-08-03 12:38:44 +0530

    added a start method to ViewServer class

--- pynotes/pynotes.py
+++ pynotes/pynotes.py
@@ -1,5 +1,8 @@
+import threading
 import socket
 import os
+
+from typing import List
 
 class ViewConnection():
 
@@ -21,6 +24,9 @@ class ViewServer(ViewConnection):
     command: str | None = None
     server: socket.socket | None = None
 
+    thread_list: List[threading.Thread] = []
+    view_lock: threading.Lock | None = None
+
     def __init__ (self, port: int | None = None, command: str | None = None, backlog: int = 4) -> None:
 
         if ViewServer.command is None:
@@ -33,6 +39,29 @@ class ViewServer(ViewConnection):
             self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.bind((socket.gethostname(), self.port))
             self.server.listen(self.backlog)
+
+        return
+
+    def thread_handler (self, client: socket.socket) -> None:
+
+        pass
+
+        return
+
+    def start (self) -> None:
+
+        connected_client: socket.socket
+        thread: threading.Thread
+
+        # TODO: this will spawn as many threads as many connections there are
+        # limit the number of threads, maybe to 20
+        while True:
+            # this will block
+            connected_client, _ = server.accept()
+
+            thread = threading.Thread(target = self.thread_handler, args = [connected_client])
+            thread.start()
+            self.thread_list.append(thread)
 
         return
 
