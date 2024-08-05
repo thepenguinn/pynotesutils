3dd05b415e67339773affa8db677a5b4d03b365c
Daniel V Mathew <danielvmathew27@gmail.com>
2024-08-05 06:15:31 +0530

    renamed the library name to pynotesutils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "pynotes",
+    name = "pynotesutils",
     packages = find_packages(["pynotes"]),
     version = "0.1.0",
     description = "A library exec and view output scripts in notes on android",
